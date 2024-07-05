1b7015066b495fa975201219d635395637a92375
eskimo204 <99628010+eskimo204@users.noreply.github.com>
2024-07-05 23:13:27 +0900

    Update streamlit_refer.py

--- streamlit_refer.py
+++ streamlit_refer.py
@@ -1,10 +1,10 @@
+pip install langchain-community
 import streamlit as st
 import tiktoken
 from loguru import logger
 
 from langchain.chains import ConversationalRetrievalChain
 from langchain.chat_models import ChatOpenAI
-pip install langchain-community
 
 from langchain.document_loaders import PyPDFLoader
 from langchain.document_loaders import Docx2txtLoader
