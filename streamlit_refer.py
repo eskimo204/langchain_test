c0b9ec220af73ed2b5554efe8de3773822278b08
eskimo204 <99628010+eskimo204@users.noreply.github.com>
2024-07-05 23:21:40 +0900

    Update streamlit_refer.py

--- streamlit_refer.py
+++ streamlit_refer.py
@@ -5,6 +5,7 @@ from loguru import logger
 
 from langchain.chains import ConversationalRetrievalChain
 from langchain.chat_models import ChatOpenAI
+from langchain_community import chat_models
 
 from langchain.document_loaders import PyPDFLoader
 from langchain.document_loaders import Docx2txtLoader
