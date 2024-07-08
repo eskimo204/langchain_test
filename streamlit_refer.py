be44fb7c9160901afaf62a1029f3264d6f9a0bf6
eskimo204 <99628010+eskimo204@users.noreply.github.com>
2024-07-08 23:06:42 +0900

    Update streamlit_refer.py

--- streamlit_refer.py
+++ streamlit_refer.py
@@ -1,4 +1,3 @@
-
 import streamlit as st
 import tiktoken
 from loguru import logger
@@ -7,7 +6,6 @@ from langchain.chains import ConversationalRetrievalChain
 from langchain.chat_models import ChatOpenAI
 from langchain_community import chat_models
 from langchain.chat_models import ChatOpenAI
-
 
 from langchain.document_loaders import PyPDFLoader
 from langchain.document_loaders import Docx2txtLoader
@@ -25,8 +23,9 @@ from langchain.memory import StreamlitChatMessageHistory
 
 def main():
     st.set_page_config(
-    page_title="DirChat",
-    page_icon=":books:")
+        page_title="DirChat",
+        page_icon=":books:"
+    )
 
     st.title("_Private Data :red[QA Chat]_ :books:")
 
@@ -40,19 +39,19 @@ def main():
         st.session_state.processComplete = None
 
     with st.sidebar:
-        uploaded_files =  st.file_uploader("Upload your file",type=['pdf','docx'],accept_multiple_files=True)
+        uploaded_files = st.file_uploader("Upload your file", type=['pdf', 'docx'], accept_multiple_files=True)
         openai_api_key = st.text_input("OpenAI API Key", key="chatbot_api_key", type="password")
         process = st.button("Process")
+    
     if process:
         if not openai_api_key:
             st.info("Please add your OpenAI API key to continue.")
             st.stop()
         files_text = get_text(uploaded_files)
         text_chunks = get_text_chunks(files_text)
-        vetorestore = get_vectorstore(text_chunks)
+        vectorstore = get_vectorstore(text_chunks)
      
-        st.session_state.conversation = get_conversation_chain(vetorestore,openai_api_key) 
-
+        st.session_state.conversation = get_conversation_chain(vectorstore, openai_api_key) 
         st.session_state.processComplete = True
 
     if 'messages' not in st.session_state:
@@ -84,13 +83,10 @@ def main():
 
                 st.markdown(response)
                 with st.expander("참고 문서 확인"):
-                    st.markdown(source_documents[0].metadata['source'], help = source_documents[0].page_content)
-                    st.markdown(source_documents[1].metadata['source'], help = source_documents[1].page_content)
-                    st.markdown(source_documents[2].metadata['source'], help = source_documents[2].page_content)
-                    
+                    for doc in source_documents:
+                        st.markdown(f"**Source:** {doc.metadata['source']}")
+                        st.markdown(f"**Content:** {doc.page_content}")
 
-
-# Add assistant message to chat history
         st.session_state.messages.append({"role": "assistant", "content": response})
 
 def tiktoken_len(text):
@@ -99,7 +95,6 @@ def tiktoken_len(text):
     return len(tokens)
 
 def get_text(docs):
-
     doc_list = []
     
     for doc in docs:
@@ -116,10 +111,8 @@ def get_text(docs):
         elif '.pptx' in doc.name:
             loader = UnstructuredPowerPointLoader(file_name)
             documents = loader.load_and_split()
-
         doc_list.extend(documents)
     return doc_list
-
 
 def get_text_chunks(text):
     text_splitter = RecursiveCharacterTextSplitter(
@@ -130,31 +123,27 @@ def get_text_chunks(text):
     chunks = text_splitter.split_documents(text)
     return chunks
 
-
 def get_vectorstore(text_chunks):
     embeddings = HuggingFaceEmbeddings(
-                                        model_name="jhgan/ko-sroberta-multitask",
-                                        model_kwargs={'device': 'cpu'},
-                                        encode_kwargs={'normalize_embeddings': True}
-                                        )  
+        model_name="jhgan/ko-sroberta-multitask",
+        model_kwargs={'device': 'cpu'},
+        encode_kwargs={'normalize_embeddings': True}
+    )
     vectordb = FAISS.from_documents(text_chunks, embeddings)
     return vectordb
 
-def get_conversation_chain(vetorestore,openai_api_key):
-    llm = ChatOpenAI(openai_api_key=openai_api_key, model_name = 'gpt-3.5-turbo',temperature=0)
+def get_conversation_chain(vectorstore, openai_api_key):
+    llm = ChatOpenAI(openai_api_key=openai_api_key, model_name='gpt-3.5-turbo', temperature=0)
     conversation_chain = ConversationalRetrievalChain.from_llm(
-            llm=llm, 
-            chain_type="stuff", 
-            retriever=vetorestore.as_retriever(search_type = 'mmr', vervose = True), 
-            memory=ConversationBufferMemory(memory_key='chat_history', return_messages=True, output_key='answer'),
-            get_chat_history=lambda h: h,
-            return_source_documents=True,
-            verbose = True
-        )
-
+        llm=llm, 
+        chain_type="stuff", 
+        retriever=vectorstore.as_retriever(search_type='mmr', verbose=True), 
+        memory=ConversationBufferMemory(memory_key='chat_history', return_messages=True, output_key='answer'),
+        get_chat_history=lambda h: h,
+        return_source_documents=True,
+        verbose=True
+    )
     return conversation_chain
-
-
 
 if __name__ == '__main__':
     main()
