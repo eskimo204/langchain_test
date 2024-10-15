d9d1783823ce2bf01532df94752d830af11f49ab
eskimo204 <99628010+eskimo204@users.noreply.github.com>
2024-10-15 23:58:13 +0900

    Update streamlit_refer.py

--- streamlit_refer.py
+++ streamlit_refer.py
@@ -55,14 +55,7 @@ def main():
 
     if 'messages' not in st.session_state:
         st.session_state['messages'] = [{"role": "assistant", 
-                                        "content": "안녕하세요! 혈당에 대해 궁금하신 것이 있으면 언제든 물어봐주세요!",
-                                       # 운영 체제 정보 출력
-                                        print(f"System: {platform.system()}"),
-                                        print(f"Node Name: {platform.node()}"),
-                                        print(f"Release: {platform.release()}"),
-                                        print(f"Version: {platform.version()}"),
-                                        print(f"Machine: {platform.machine()}"),
-                                        print(f"Processor: {platform.processor()}")}]
+                                        "content": "안녕하세요! 혈당에 대해 궁금하신 것이 있으면 언제든 물어봐주세요!"}]
 
     for message in st.session_state.messages:
         st.write(message)
